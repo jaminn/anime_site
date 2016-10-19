47daf7fdfbdc36d432f25ecf53c3adafbc6542df
storyfree <rnnwkals1@naver.com>
2016-10-19 11:33:05 +0900

    Before python study_may_finale

--- get_anigod_video.py
+++ get_anigod_video.py
@@ -1,21 +1,33 @@
 import requests, re
+import urllib.request
 from urllib.parse import quote, unquote
+from bs4 import BeautifulSoup
 import execjs
 
 
 def get_anigod_video(past_url):
     url = unquote(past_url)
-    headers = {'referer': 'http://t.umblr.com/'}
+    print(url)
     patt = r"var videoID = ?'(.*?)';"
-    videoID =[]
-    while not videoID:
-        text = requests.get(url, headers=headers).text
-        videoID = re.findall(patt, text)
+    hdr = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive',
+        'referer': 'http://t.umblr.com/'}
+    videoID = []
+    req = urllib.request.Request(url, headers=hdr)
+    data = urllib.request.urlopen(req)
+    soup = BeautifulSoup(data)
+    text = soup.get_text()
+    videoID = re.findall(patt, text)
     ID = execjs.eval('encodeURIComponent("' + videoID[0] + '")')
     return 'https://anigod.com/video?id=' + ID
     # return 'https://anigod.com/video?id='+quote(videoID[0],safe='')
 
 
 if __name__ == "__main__":
-    url = "https://anigod.com/episode/%ea%b0%95%ec%b2%a0-%ec%98%a4%ec%bc%80%ec%8a%a4%ed%8a%b8%eb%9d%bc-2%ed%99%94-29549"
-    print(get_anigod_video(url))
+    url = "https://anigod.com/episode/%EB%B0%94%EB%82%98%EB%83%90-4%ED%99%94-23940"
+    print(get_anigod_video(quote(url,safe='')))
