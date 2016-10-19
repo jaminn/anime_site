5844e76c4fd3351af9b130999d002d55d9455a8e
storyfree <rnnwkals1@naver.com>
2016-10-19 13:18:03 +0900

    Before python study_may_finale

--- get_anigod_video.py
+++ get_anigod_video.py
@@ -10,7 +10,7 @@ def get_anigod_video(past_url):
     print(url)
     patt = r"var videoID = ?'(.*?)';"
     hdr = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'User-Agent': 'Mozilla/5.0',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
         'Accept-Encoding': 'none',
